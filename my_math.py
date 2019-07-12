e386a6c04272f64086b408ea4ba78e1da5e6adbd
naa45 <naa4550@gmail.com>
2019-07-12 10:14:56 +0000

    this is the first push

---
+++ my_math.py
@@ -0,0 +1,1 @@
+def calculate("Divide",num1,num2):
--- split.py
+++ split.py
@@ -3,3 +3,11 @@ data = sentence.split()
 
 for temp in data:
     print (temp)
+    
+x="mary had a little lamb"
+
+print(x.split('a'))
+hi = "hello mother ,\n hello father ."
+print(hi.split())
+print(hi.split("\n"))
+
